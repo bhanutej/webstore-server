f8dff0eaf90442e105ab4533dab569a9f82578f8
bhanutejgitlab <bhanutej.kalla89@gmail.com>
2023-03-30 08:53:26 +0530

    news letter mutations and queries

--- graphql/resolvers/newsSubscriber.js
+++ graphql/resolvers/newsSubscriber.js
@@ -3,6 +3,14 @@ const { AuthenticationError } = require('apollo-server-express');
 const { NewsSubscriber } = require('../../database/models');
 
 module.exports = {
+
+  Query: {
+    async newsLetterEmails(root, args, context) {
+      const { limit } = args;
+      return await NewsSubscriber.findAll({ limit });
+    }
+  },
+
   Mutation: {
     async addNewsSubscriber(root, args, { user = null }) {
       const { email } = args.input;
@@ -13,6 +21,18 @@ module.exports = {
       await NewsSubscriber.create({ email });
       return {
         message: "Thank you for your subscription"
+      }
+    },
+
+    async deleteNewsSubscriber(root, args, { user = null }) {
+      const { id } = args.input;
+      const newsSubscriber = await NewsSubscriber.findOne({ where: { id } });
+      if (!newsSubscriber) {
+        throw new AuthenticationError('Subscriber not found!');
+      }
+      await newsSubscriber.destroy();
+      return {
+        message: "Delted Successfully!"
       }
     }
   }
--- graphql/schemas/newsSubscriber.js
+++ graphql/schemas/newsSubscriber.js
@@ -7,13 +7,27 @@ module.exports = gql`
 
   extend type Mutation {
     addNewsSubscriber(input: NewsSubscriberInput!): NewsSubscriberResponse!
+    deleteNewsSubscriber(input: DeleteSubscriberInput!): NewsSubscriberResponse!
+  }
+
+  extend type Query {
+    newsLetterEmails(limit: Int!): [NewsSubscriberEmailListResponse!]!
   }
 
   input NewsSubscriberInput {
     email: String!
   }
+  
+  input DeleteSubscriberInput {
+    id: Int!
+  }
 
   type NewsSubscriberResponse {
     message: String!
+  }
+  
+  type NewsSubscriberEmailListResponse {
+    id: Int!
+    email: String!
   }
 `;
